8e62ace9e8d148e4936c115a49f7c77d29c6308e
Tania873 <tania.kostyuk01@gmail.com>
2022-11-08 16:30:59 +0200

    дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+    const galleryCard = `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    
+    return galleryCard;
+}
+
+const galleryCardsArr = galleryItems.map(el => makeGalleryCard(el));
+
+galleryListEl.insertAdjacentHTML("afterbegin", galleryCardsArr.join(''));
+
+const onGalleryImgClick = event => {
+    event.preventDefault();
+
+        const { target } = event;
+
+    if (target.nodeName !== 'IMG') {
+        return;
+    }
+
+    new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+     });
+}
+
+galleryListEl.addEventListener('click', onGalleryImgClick);
