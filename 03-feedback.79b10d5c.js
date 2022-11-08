894077434c919efc39388135f9ecf372150f3da9
Tania873 <106867802+Tania873@users.noreply.github.com>
2022-11-08 14:31:28 +0000

    Deploying to gh-pages from @ Tania873/goit-js-hw-08@8e62ace9e8d148e4936c115a49f7c77d29c6308e 🚀

---
+++ 03-feedback.79b10d5c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form-state");console.log("feedbackFormEl");e.addEventListener("input",(e=>{console.log(e.target)}));
+//# sourceMappingURL=03-feedback.79b10d5c.js.map
