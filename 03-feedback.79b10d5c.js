602260b3671e7d1711cb47068e01f1cca087f758
Tania873 <106867802+Tania873@users.noreply.github.com>
2022-11-09 18:45:30 +0000

    Deploying to gh-pages from @ Tania873/goit-js-hw-08@6bdd86c0bdf857e97528dff5888850ec35b4aaf0 🚀

--- 03-feedback.79b10d5c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form-state");console.log("feedbackFormEl");e.addEventListener("input",(e=>{console.log(e.target)}));
-//# sourceMappingURL=03-feedback.79b10d5c.js.map
--- 03-feedback.a1804fe9.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form-state");console.log("feedbackFormEl");e.addEventListener("input",(function(e){console.log(e.target)}))}();
-//# sourceMappingURL=03-feedback.a1804fe9.js.map
